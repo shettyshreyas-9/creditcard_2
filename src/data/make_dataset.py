82ff56476761ab96324544e8b19d0bb1292f3a87
shettyshreyas-9 <shreyasshetty925@gmail.com>
2024-01-11 15:42:48 +0530

    modified dvc.yaml stages,requirements,created  make_dataset code & executed it which created output data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -11,19 +11,43 @@ def load_data(data_path):
     df = pd.read_csv(data_path)
     return df
 
-
+def save_data(data,output_path):
+    sdf= data.to_csv(output_path+'/train.csv')
 
 def main():
 
     curr_dir = pathlib.Path(__file__)
     home_dir = curr_dir.parent.parent.parent
 
-    input_file = sys.argv[1]
+    dvc_yaml = yaml.safe_load(open('dvc.yaml'))['stages']
+
+    input_file= sys.argv[1]
     data_path = home_dir.as_posix() + input_file
+
+    
+    # input_file1 = dvc_yaml['make_dataset']['deps'][0]
+    # input_file2 = '/data/raw/creditcard.csv'
+    # data_path2 = home_dir.as_posix() + input_file2
+    # data_path1 = home_dir.as_posix() + input_file1
+    
+    output_path = home_dir.as_posix() + '/data/processed'
 
     data = load_data(data_path)
 
-    return data
+    save_data(data,output_path)
+
+    #return data
+
+    # print(curr_dir)
+    # print(home_dir)
+    # print(data_path2)
+    # print(input_file1)
+
+    # print(input_file2)
+    print('',data_path,'', sep='\n')
+    print('',output_path,'', sep='\n' )
+
+    # print(input_file)
 
 if __name__ == "__main__":
     main()
