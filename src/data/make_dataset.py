57d03c2d033ccd3cac199ca7fcc9957cf5b5667d
shettyshreyas-9 <shreyasshetty925@gmail.com>
2024-01-11 17:38:16 +0530

    code for spliting data to train & test then save it. Made resp changes in dvc.yaml & added params.yaml

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -11,43 +11,45 @@ def load_data(data_path):
     df = pd.read_csv(data_path)
     return df
 
-def save_data(data,output_path):
-    sdf= data.to_csv(output_path+'/train.csv')
+def split_data(df, test_split,seed):
+    train, test = train_test_split(df, test_size=test_split, random_state=seed)
+    return train, test
+
+def save_data(train, test,output_path):
+    pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
+    train.to_csv(output_path +'/train.csv', index=False)
+    test.to_csv(output_path +'/test.csv', index=False)
 
 def main():
 
     curr_dir = pathlib.Path(__file__)
     home_dir = curr_dir.parent.parent.parent
 
-    dvc_yaml = yaml.safe_load(open('dvc.yaml'))['stages']
+    params_file = home_dir.as_posix()+ '/params.yaml'
+    # params= yaml.safe_load(open(params_file))["make_dataset"]
+    params = yaml.safe_load(open(params_file))["make_dataset"]
 
     input_file= sys.argv[1]
     data_path = home_dir.as_posix() + input_file
-
-    
-    # input_file1 = dvc_yaml['make_dataset']['deps'][0]
-    # input_file2 = '/data/raw/creditcard.csv'
-    # data_path2 = home_dir.as_posix() + input_file2
-    # data_path1 = home_dir.as_posix() + input_file1
     
     output_path = home_dir.as_posix() + '/data/processed'
 
-    data = load_data(data_path)
-
-    save_data(data,output_path)
-
-    #return data
-
-    # print(curr_dir)
-    # print(home_dir)
-    # print(data_path2)
-    # print(input_file1)
-
-    # print(input_file2)
     print('',data_path,'', sep='\n')
     print('',output_path,'', sep='\n' )
+    print('',params_file,'', sep='\n' )
+    # print('',params,'', sep='\n' )
 
-    # print(input_file)
+    #load data
+    data = load_data(data_path)
+
+
+    # split data
+    train_data, test_data = split_data(data,params['test_split'],params['seed'])
+
+    # save data
+    save_data(train_data, test_data,output_path)
+    
+
 
 if __name__ == "__main__":
     main()
