035137e2fc677c8cbbcc87418b2202f63852306f
shettyshreyas-9 <shreyasshetty925@gmail.com>
2024-01-11 15:40:43 +0530

    modified dvc.yaml stages,requirements,created  make_dataset code & executed it which created output data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,30 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pathlib
+import yaml
+import sys
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+
+def load_data(data_path):
+    # Load your dataset from a given path
+    df = pd.read_csv(data_path)
+    return df
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+def main():
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    curr_dir = pathlib.Path(__file__)
+    home_dir = curr_dir.parent.parent.parent
 
+    input_file = sys.argv[1]
+    data_path = home_dir.as_posix() + input_file
+
+    data = load_data(data_path)
+
+    return data
+
+if __name__ == "__main__":
     main()
+
